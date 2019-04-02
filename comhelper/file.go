2204e567b5d2699a702af107ff6e9924a6569e1f
jiangyj <jiangyj@zjmax.com>
2019-04-02 17:36:41 +0800

    添加文件处理函数

--- comhelper/file.go
+++ comhelper/file.go
@@ -1,6 +1,11 @@
 package comhelper
 
-import "os"
+import (
+	"bufio"
+	"encoding/base64"
+	"log"
+	"os"
+)
 
 /**
  * 检查文件夹是否存在
@@ -14,4 +19,29 @@ func IsDir(path string) (bool, error) {
 		return false, nil
 	}
 	return false, err
+}
+
+/**
+ * 将图片转换为base64格式
+ */
+func ImgToBase64(path string) (string, error) {
+	imgFile, err := os.Open(path)
+	if err != nil {
+		log.Println(err)
+	}
+
+	defer imgFile.Close()
+
+	//create a new buffer base on file size
+	fInfo, _ := imgFile.Stat()
+	var size int64 = fInfo.Size()
+	buf := make([]byte, size)
+
+	//read file content into buffer
+	fReader := bufio.NewReader(imgFile)
+	fReader.Read(buf)
+
+	//convert the buffer bytes to base64 string - use buf.Bytes() for new image
+	imgBase64str := base64.StdEncoding.EncodeToString(buf)
+	return "data:image/jpeg;base64," + imgBase64str, nil
 }
